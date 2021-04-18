64515ceb4abdec0f7fafbc0e9b3705943f1d3036
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-04-18 21:09:17 +0600

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,7 @@ import "firebase/auth";
 import firebaseConfig from './firebase.config/firebase.config'
 import { useHistory, useLocation } from 'react-router';
 import { GlobalContext } from '../../App';
+import './Login.css'
 
 
 
@@ -63,17 +64,21 @@ const Google = () => {
         });
   }
     return (
-        <div>
-          {
-            user.isSignedIn ? <button className="btn" onClick={handleGoogleOut}>Logout</button> : <button className="btn" onClick={handleGoogleIn}>Login with Google</button>
+        <section className="design-container">
+          <div >
+          <div className="col-md-4 col-sm-1"></div>
+          <div className="col-md-4 col-sm-1 login-container">{
+            user.isSignedIn ? <button className="btn" onClick={handleGoogleOut}>Logout</button> : <button className="btn btn-info" onClick={handleGoogleIn}>Login with Google</button>
             }
            {
                user.isSignedIn &&  <div><h3>{user.name}</h3>
                 <p>{user.email}</p>
                 <img src={user.photo} alt=""/>
                </div>
-           } 
+           } </div>
+          <div className="col-md-4 col-sm-1"></div>
         </div>
+        </section>
     );
 };
 
