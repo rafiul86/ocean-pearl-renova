b366491ae00e7a2e1963c7f71d36ff1238d50b99
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-04-18 13:49:01 +0600

    dashboard completed

--- src/components/Dashboard/Admin/RemoveService/RemoveService.js
+++ src/components/Dashboard/Admin/RemoveService/RemoveService.js
@@ -1,5 +1,7 @@
+import { Grid } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import Service from '../../../Home/Services/Service/Service';
+import Sidebar from '../../Sidebar/Sidebar';
 import ServiceData from './ServiceData/ServiceData';
 
 const RemoveService = () => {
@@ -19,10 +21,18 @@ const RemoveService = () => {
         })
     }
     return (
-        <div>
-            {
-                serviceList.map(service =><ServiceData handleDelete={handleDelete} service={service}></ServiceData>)
-            }
+
+        <div className="row">
+            <div className="col-md-2">
+                <Sidebar/>
+            </div>
+            <div className="col-md-9">
+        
+                <Grid container spacing={6}>{
+                    serviceList.map(service =><Grid item xs={10} md={6} lg={4}><ServiceData handleDelete={handleDelete} service={service}></ServiceData></Grid>)
+                }</Grid> 
+ 
+            </div>
         </div>
     );
 };
--- src/components/Dashboard/Admin/RemoveService/ServiceData/ServiceData.js
+++ src/components/Dashboard/Admin/RemoveService/ServiceData/ServiceData.js
@@ -1,13 +1,18 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import Sidebar from '../../../Sidebar/Sidebar';
 
 const ServiceData = (props) => {
     const {handleDelete , service} = props
     return (
-        <div>
-            <h1>{service.name}</h1>
-            <Button onClick={()=> handleDelete(service._id)} >DELETE</Button>
-        </div>
+            <div className="card" style={{width: "18rem;"}}>
+  <img className="card-img-top" style={{height : '250px' , width : '300px'}} src={`https://vast-journey-70627.herokuapp.com/${service.img}`} alt=""/>
+  <div className="card-body">
+    <h5 className="card-title">Service : {service.name}</h5>
+<p>Service Price: ${service.price}</p>
+    <Button variant="contained" color="secondary" onClick={()=> handleDelete(service._id)} >DELETE</Button>
+  </div>
+</div>
     );
 };
 
